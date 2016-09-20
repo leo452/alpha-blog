3fb6c504f6b8f65912e8414e733098c2b3a93e76
Leonardo merchan <leonardo.merchan.1992@gmail.com>
2016-09-20 03:24:56 +0000

    Add users listing and updates show articles view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
     
+    
+  
+    
     def new
     @user = User.new
     end
+    
+    def index
+    @users = User.all
+    end
+    
     def create
         @user = User.new(user_params)
         if @user.save
