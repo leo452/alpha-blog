dd690b32cc080700decdea6d4dde7c26a25c06b2
Leonardo merchan <leonardo.merchan.1992@gmail.com>
2016-09-20 11:53:15 +0000

    Arreglo #1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,11 @@ class ApplicationController < ActionController::Base
   end
   
   def require_user
+    if !logged_in?
+    
     flash[:danger] = "you must be logged in to perform that action "
     redirect_to root_path
+    end
   end
 
 end
